eafb0ac886d6ee45e81fd7c2d3458a19b6261c87
jinhee yoo <epuni1717@naver.com>
2021-05-08 14:14:48 +0900

    mongodb 저장하는 API 추가

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # 플라스크 웹 서버 생성하기
 app = Flask(__name__)
@@ -12,9 +12,35 @@ def index():   # 함수 이름은 고유해야 한다
 
 # app.py 파일을 직접 실행시킬 때 동작시킴
 # 이 코드가 가장 아랫부분
-if __name__ == '__main__':
+if __name__ == '__main__'p
     app.run(
         '0.0.0.0',  # 모든 IP에서 오는 요청을 허용
         7000,  # 플라스크 웹 서버는 7000번 포트 사용
         debug=True,  # 에버 발생 시 에러 로그 보여줌
     )
+
+
+# 아티클 추가 API
+@app.route('/memo', methods=['POST'])
+def save_memo():
+    from = request.form
+    url_receive = form['url_give']
+    comment_receive = form['comment_give']
+
+    print(url_receive, comment_receive
+    document = {
+        'url': url_receive,
+        'comment': comment_receive,
+    }
+    db.articles.insert_one(document)
+    return jsonify(
+        {'result': 'success'}
+    )
+
+
+# mongodb 추가
+client = MongoClient('localhost', 27017)
+db = client.
+
+    
+    
