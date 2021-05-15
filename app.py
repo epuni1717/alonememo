51c8d267bec9f09f9bacc74bac5ce1e97f9c9f77
jinhee yoo <epuni1717@naver.com>
2021-05-15 10:36:43 +0900

    로그인 페이지 구현

--- app.py
+++ app.py
@@ -12,30 +12,54 @@ def index():   # 함수 이름은 고유해야 한다
 
 # app.py 파일을 직접 실행시킬 때 동작시킴
 # 이 코드가 가장 아랫부분
-if __name__ == '__main__'p
+if __name__ == '__main__'
     app.run(
         '0.0.0.0',  # 모든 IP에서 오는 요청을 허용
         7000,  # 플라스크 웹 서버는 7000번 포트 사용
         debug=True,  # 에버 발생 시 에러 로그 보여줌
     )
 
+@app.route('/memo', methods=['GET'])
+def list_login():
+    return render_template('login.html')
+
 
 # 아티클 추가 API
 @app.route('/memo', methods=['POST'])
-def save_memo():
+def save_memo(url_receive=None):
     from = request.form
     url_receive = form['url_give']
     comment_receive = form['comment_give']
 
-    print(url_receive, comment_receive
+    headers = {
+        'User-Agent':
+    }
+    response = requests.get(
+        url_receive,
+        headers=headers
+    )
+    soup = BeautifulSoup(response.text,'html.parser')
+
+    title = soup.select_one('meta[property="og:title"]')
+    url = soup.select_one('meta[property="og:description"]')
+    image = soup.select_one('meta[property')
+    print(description)
+    print(title['content'])
+    print(url['content'])
+
+
     document = {
         'url': url_receive,
         'comment': comment_receive,
     }
     db.articles.insert_one(document)
     return jsonify(
-        {'result': 'success'}
+        {'result': 'success', 'msg': '저장했습니다.'}
     )
+
+
+
+
 
 
 # mongodb 추가
